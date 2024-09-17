67387ece72d9677cb8ee74ab98ef00b3b21d2b7d
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-17 12:57:39 -0500

    feat: :sparkles: Initial configuration of the project

--- server/dto/userDto.js
+++
@@ -1,9 +0,0 @@
-class UserDTO {
-  constructor({ nombre, email, contrasena }) {
-    this.n = nombre;
-    this.e = email;
-    this.c = contrasena;
-  }
-}
-
-module.exports = UserDTO;
--- server/model/userModel.js
+++
@@ -1,5 +0,0 @@
-module.exports = class User{
-    findOneById({n, e, c}){
-        return {n, e, c}
-    }
-}
