a18edd55c2fa699d706b845abf36bcfd2782a15c
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-23 13:01:04 -0500

    feat: :construction: Discord auth in progress

--- server/src/middlewares/passport/discordStrategy.js
+++ server/src/middlewares/passport/discordStrategy.js
@@ -9,23 +9,44 @@ const userService = new UserService()
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID
 const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET
 
+// {
+//     id: '1194669787205341185',
+//     username: 'mariana1827',
+//     avatar: '0d90264f2f1b7e518817e54243dcffd0',
+//     discriminator: '0',
+//     public_flags: 0,
+//     flags: 0,
+//     banner: null,
+//     accent_color: null,
+//     global_name: 'Mariana Traslaviña Sarmiento',
+//     avatar_decoration_data: null,
+//     banner_color: null,
+//     clan: null,
+//     mfa_enabled: false,
+//     locale: 'en-US',
+//     premium_type: 0,
+//     email: 'mariana.traslavina18@gmail.com',
+//     verified: true,
+//     provider: 'discord',
+//     accessToken: '8Qhk9KHJH9XWc3YI99dsBQu26kEJa4',
+//     fetchedAt: 2024-09-23T17:58:07.399Z
+//   }
 
 module.exports = new DiscordStrategy({
     clientID: DISCORD_CLIENT_ID,
     clientSecret: DISCORD_CLIENT_SECRET,
     callbackURL: '/api/auth/discord/callback',
-    scope: ['identify', 'email']
+    scope: ["identify", "email"],
 
 }, async(accessToken, refreshToken, profile, done) => {
     console.log(profile)
     try {
+        let user = await userService.getOne({discord_id: profile.id})
         if (user) {
             return done(null, user);
         } else {
             user = new UserModel({
                 discord_id: profile.id,    
-                username: profile.username,
-                provider: 'discord'
             });
             await user.save();
             return done(null, user);
