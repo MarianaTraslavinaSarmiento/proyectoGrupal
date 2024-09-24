3d56f331ec712c66bfaec4a0a84aad40e4ab16ec
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-24 07:39:32 -0500

    feat: :sparkles: workshops grid design completed

--- vue-app/src/views/workshops-crafts-stores/WorkshopsAndCraftStores.vue
+++ vue-app/src/views/workshops-crafts-stores/WorkshopsAndCraftStores.vue
@@ -9,16 +9,58 @@ import TitleSection from '@components/title-section/TitleSection.vue'
     <HeadBar />
     <main>
 
-        <TitleSection
-        title="Talleres y tiendas artesanales"
-        subtitle="Tiendas de artesanías de todas partes del Perú"
-        :showFilter="true"
-        />
+        <TitleSection title="Talleres y tiendas artesanales" subtitle="Tiendas de artesanías de todas partes del Perú"
+            :showFilter="true" />
+
+        <div class="workshops__container">
+            <div class="item">
+                <div class="item__title">
+                    <h5>Arte Abedail Aller Escalante</h5>
+                    <p>Cusco</p>
+                </div>
+                <div class="item__image">
+                    <img src="/public/img/workshopOne.png" alt="">
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__title">
+                    <h5>Arte Abedail Aller Escalante</h5>
+                    <p>Cusco</p>
+                </div>
+                <div class="item__image">
+                    <img src="/public/img/workshopOne.png" alt="">
+                </div>
+            </div>
+        </div>
 
     </main>
     <Nav />
 </template>
 
 <style lang="scss" scoped>
+.workshops__container {
+    font-family: Bellota;
+    display: flex;
+    padding: 15px 20px;
+}
 
+.item {
+    max-width: 180px;
+    color: var(--color-black);
+    border-radius: 5px;
+
+    &__title {
+        background-color: var(--background-secondary);
+        padding: 5px;
+        border-radius: 5px 5px 0px 0px;
+        font-size: 1.2rem;
+        color: var(--text-color);
+
+        h5 {
+            margin: 0px;
+        }
+
+    }
+
+}
 </style>
