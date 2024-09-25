d7efc3f9c3a1f3e51065f83243173023aa35d32d
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-25 13:04:32 -0500

    feat: :construction: Carousel component in progress

---
+++ vue-app/src/components/categories-carousel/CategoriesCarousel.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+import HeadBar from '../head-bar/components/HeadBar.vue';
+import Nav from '../nav/Nav.vue';
+import TitleSection from '../title-section/TitleSection.vue'
+import Location from '@assets/img/categories/Location.vue'
+import Textile from '@assets/img/categories/Textile.vue';
+import Ceramics from '@assets/img/categories/Ceramics.vue'
+import Goldsmithing from '@assets/img/categories/Goldsmithing.vue'
+import StoneCarving from '@assets/img/categories/StoneCarving.vue'
+import WoodCarving from '@assets/img/categories/WoodCarving.vue'
+import Embroidery from '@assets/img/categories/Embroidery.vue'
+import Jewelry from '@assets/img/categories/Jewelry.vue'
+import SheetMetal from '@assets/img/categories/SheetMetal.vue'
+import PaintingTraditiona from '@assets/img/categories/PaintingTraditiona.vue'
+import Printed from '@assets/img/categories/Printed.vue'
+
+</script>
+
+<template>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
