19ed6ffeecc7b11a6f3247185574ebdde73cb69e
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-26 09:23:51 -0500

    feat: :sparkles: Products container created

--- vue-app/src/components/categories-carousel/CategoriesCarousel.vue
+++ vue-app/src/components/categories-carousel/CategoriesCarousel.vue
@@ -1,91 +1,92 @@
 <script setup>
-
-
-import Nav from '../nav/Nav.vue';
-import TitleSection from '../title-section/TitleSection.vue'
-import Location from '@assets/img/categories/Location.vue'
-import Textile from '@assets/img/categories/Textile.vue';
-import Ceramics from '@assets/img/categories/Ceramics.vue'
-import Goldsmithing from '@assets/img/categories/Goldsmithing.vue'
-import StoneCarving from '@assets/img/categories/StoneCarving.vue'
-import WoodCarving from '@assets/img/categories/WoodCarving.vue'
-import Embroidery from '@assets/img/categories/Embroidery.vue'
-import Jewelry from '@assets/img/categories/Jewelry.vue'
-import SheetMetal from '@assets/img/categories/SheetMetal.vue'
-import PaintingTraditiona from '@assets/img/categories/PaintingTraditiona.vue'
-import Printed from '@assets/img/categories/Printed.vue'
-
+import Nav from "../nav/Nav.vue";
+import TitleSection from "../title-section/TitleSection.vue";
+import Location from "@assets/img/categories/Location.vue";
+import Textile from "@assets/img/categories/Textile.vue";
+import Ceramics from "@assets/img/categories/Ceramics.vue";
+import Goldsmithing from "@assets/img/categories/Goldsmithing.vue";
+import StoneCarving from "@assets/img/categories/StoneCarving.vue";
+import WoodCarving from "@assets/img/categories/WoodCarving.vue";
+import Embroidery from "@assets/img/categories/Embroidery.vue";
+import Jewelry from "@assets/img/categories/Jewelry.vue";
+import SheetMetal from "@assets/img/categories/SheetMetal.vue";
+import PaintingTraditiona from "@assets/img/categories/PaintingTraditiona.vue";
+import Printed from "@assets/img/categories/Printed.vue";
 </script>
 
 <template>
-
-  <div class="categories">
-    <div class="categories__item">
-      <div class="categories__circle">
-        <Textile class="categories__icon" />
+  <main>
+    <div class="categories">
+      <div class="categories__item">
+        <div class="categories__circle">
+          <Textile class="categories__icon" />
+        </div>
+        <p>Textilería</p>
       </div>
-      <p>Textilería</p>
-    </div>
-    <div class="categories__item">
-      <div class="categories__circle">
-        <Ceramics class="categories__icon" />
+      <div class="categories__item">
+        <div class="categories__circle">
+          <Ceramics class="categories__icon" />
+        </div>
+        <p>Cerámica</p>
       </div>
-      <p>Cerámica</p>
-    </div>
-    <div class="categories__item">
-      <div class="categories__circle">
-        <Goldsmithing class="categories__icon" />
+      <div class="categories__item">
+        <div class="categories__circle">
+          <Goldsmithing class="categories__icon" />
+        </div>
+        <p>Orfebrería</p>
       </div>
-      <p>Orfebrería</p>
-    </div>
-    <div class="categories__item">
-      <div class="categories__circle">
-        <StoneCarving class="categories__icon" />
+      <div class="categories__item">
+        <div class="categories__circle">
+          <StoneCarving class="categories__icon" />
+        </div>
+        <p>Orfeberia</p>
       </div>
-      <p>Orfeberia</p>
-    </div>
-    <div class="categories__item">
-      <div class="categories__circle">
-        <WoodCarving class="categories__icon" />
+      <div class="categories__item">
+        <div class="categories__circle">
+          <WoodCarving class="categories__icon" />
+        </div>
+        <p>Orfeberia</p>
       </div>
-      <p>Orfeberia</p>
     </div>
-  </div>
-
+  </main>
 </template>
 
 <style lang="scss" scoped>
+
+
 .categories {
   display: flex;
   overflow-x: auto;
   gap: 1.5rem;
   scroll-behavior: smooth;
   padding-left: 1.7rem;
-  padding-top: 1.5rem;  
-  padding-bottom: 1rem;
+  padding-top: 1.5rem;
   scrollbar-width: none;
   padding-inline-end: 1.5rem;
   border-bottom: 2px solid var(--background-secondary);
+  font-family: Bellota;
+
 
   &::-webkit-scrollbar {
-  display: none;
-}
-    &__item {
-      display: flex;
-      flex-direction: column;
-      justify-content: start;
-      align-items: center ;
-      gap: 5px;
-      p {
-        font-size: 1.6rem;
-        text-align: center;
-        color: var(--text-contrast);
-      }
+    display: none;
+  }
+  &__item {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    gap: 5px;
+    p {
+      font-size: 1.4rem;
+      text-align: center;
+      color: var(--color-black);
+      padding-bottom: 1rem;
     }
-    &__circle {
+  }
+  &__circle {
     flex: 0 0 auto;
-    width: 60px;
-    height: 60px;
+    width: 50px;
+    height: 50px;
     border-radius: 50%;
     background-color: var(--background-secondary);
     display: flex;
@@ -94,8 +95,8 @@ import Printed from '@assets/img/categories/Printed.vue'
     margin: 0 10px;
   }
   &__icon {
-    width: 35px;
-    height: 40px;
+    width: 30px;
+    height: 35px;
   }
 }
 </style>
