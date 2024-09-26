0a9f680d4d3e6c304d34b9f4618a7c6bde73d087
DaroOps <romerosd089@hotmail.com>
2024-09-26 12:19:33 -0500

    feat: :sparkles: RUR-39 initial config

--- vue-app/src/components/avatar/Avatar.vue
+++ vue-app/src/components/avatar/Avatar.vue
@@ -1,31 +1,51 @@
 <script setup>
-    import {computed} from 'vue'
-    import { useUserStore } from '@stores/user.js'
+import { computed } from 'vue'
+import { useUserStore } from '@stores/user.js'
+import  EditIcon  from '@icons/profile/EditIcon.vue'
 
-    const userStore = useUserStore()
+const props = defineProps({
+  showEditIcon: {
+    type: Boolean,
+    default: false
+  }
+})
 
-    const user = computed(() => userStore.user)
+const userStore = useUserStore()
+const user = computed(() => userStore.user)
 </script>
 
 <template>
-    <div class="avatar">
-        <img :src="user.avatar" :alt="user.name" class="avatar-img">
-    </div>
+  <div class="avatar">
+    <img :src="user.avatar" :alt="user.name" class="avatar-img">
+    <EditIcon v-if="showEditIcon" class="edit-icon" />
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    .avatar {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        overflow: hidden;
-        border: 2.5px solid var(--color-border);
+.avatar {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 2.5px solid var(--color-border);
 
-        .avatar-img {
-            aspect-ratio: 1;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
-    }
+  .avatar-img {
+    aspect-ratio: 1;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .edit-icon {
+    position: absolute;
+    top: 40%;
+    left: 42%;
+
+    color: var(--color-black);
+    padding: 4px;
+    width: 18%;
+    cursor: pointer;
+  }
+}
 </style>
