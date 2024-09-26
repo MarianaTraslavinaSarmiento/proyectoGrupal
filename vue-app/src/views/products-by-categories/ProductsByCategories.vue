4fa90bf5a87f3a6f12a5d3ea334f2e77bad9858a
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-26 08:01:30 -0500

    style: :lipstick: Carousel design

--- vue-app/src/views/products-by-categories/ProductsByCategories.vue
+++ vue-app/src/views/products-by-categories/ProductsByCategories.vue
@@ -1,10 +1,13 @@
 <script setup>
 
 import CategoriesCarousel from "@components/categories-carousel/CategoriesCarousel.vue"
+import HeaderTitle from "@/components/header-title/HeaderTitle.vue";
 
 </script>
 
 <template>
+    <HeaderTitle
+    title="Categorías"/>
     <CategoriesCarousel/>
 </template>
 
