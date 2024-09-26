8531f3e8459f675ad66c2080cebfdb7f7f3ecb31
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-09-26 07:39:22 -0500

    feat: :construction: Carousel component personalization in progress

--- vue-app/src/components/categories-carousel/CategoriesCarousel.vue
+++ vue-app/src/components/categories-carousel/CategoriesCarousel.vue
@@ -1,7 +1,6 @@
 <script setup>
 
 
-import HeadBar from '../head-bar/components/HeadBar.vue';
 import Nav from '../nav/Nav.vue';
 import TitleSection from '../title-section/TitleSection.vue'
 import Location from '@assets/img/categories/Location.vue'
@@ -20,10 +19,81 @@ import Printed from '@assets/img/categories/Printed.vue'
 
 <template>
 
-
+  <div class="categories">
+    <div class="categories__item">
+      <div class="categories__circle">
+        <Textile class="categories__icon" />
+      </div>
+      <p>Textilería</p>
+    </div>
+    <div class="categories__item">
+      <div class="categories__circle">
+        <Ceramics class="categories__icon" />
+      </div>
+      <p>Cerámica</p>
+    </div>
+    <div class="categories__item">
+      <div class="categories__circle">
+        <Goldsmithing class="categories__icon" />
+      </div>
+      <p>Orfebrería</p>
+    </div>
+    <div class="categories__item">
+      <div class="categories__circle">
+        <StoneCarving class="categories__icon" />
+      </div>
+      <p>Talla en piedra</p>
+    </div>
+    <div class="categories__item">
+      <div class="categories__circle">
+        <WoodCarving class="categories__icon" />
+      </div>
+      <p>Talla en madera</p>
+    </div>
+  </div>
 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.categories {
+  display: flex;
+  overflow-x: auto;
+  gap: 1.5rem;
+  scroll-behavior: smooth;
+  margin-left: 1.7rem;
+  scrollbar-width: none;
+  padding-inline-end: 1.5rem;
+  margin-bottom: 10px;
 
+  &::-webkit-scrollbar {
+  display: none;
+}
+    &__item {
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      align-items: center ;
+      gap: 5px;
+      p {
+        font-size: 1.6rem;
+        text-align: center;
+        color: var(--text-contrast);
+      }
+    }
+    &__circle {
+    flex: 0 0 auto;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    background-color: var(--background-secondary);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 10px;
+  }
+  &__icon {
+    width: 35px;
+    height: 40px;
+  }
+}
 </style>
---
+++ vue-app/src/views/products-by-categories/ProductsByCategories.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import CategoriesCarousel from "@components/categories-carousel/CategoriesCarousel.vue"
+
+</script>
+
+<template>
+    <CategoriesCarousel/>
+</template>
+
+<style scoped>
+
+p {
+    color: black
+}
+
+</style>
