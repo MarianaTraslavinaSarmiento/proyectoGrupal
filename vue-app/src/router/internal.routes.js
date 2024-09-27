0ba6323e86a277905cd6bdaade8f8d217e901f3b
DaroOps <romerosd089@hotmail.com>
2024-09-27 10:31:36 -0500

    feat: :adhesive_bandage: Small route changes

--- vue-app/src/router/internal.routes.js
+++ vue-app/src/router/internal.routes.js
@@ -64,7 +64,7 @@ export const internalRoutes = [
         component: CommentsToTheApp
     },
     {
-        path: '/app-settings/CustomerService',
+        path: '/app-settings/customer-service',
         name: 'CustomerService',
         alias: 'atencion-al-cliente',
         component: CustomerService
