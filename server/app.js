9d7969ee260eadbf09e9b3d789e81aa162e9d025
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-09-28 20:41:07 -0500

    refactor: :recycle: Start server

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const configureMiddleware = require("./src/config/configureMiddleware");
 const configureRoutes = require("./src/config/configureRoutes");
 const errorHandler = require("./src/middlewares/errorHandler");
+const {startServer} = require("./src/server");
 
 const app = express();
 
 configureMiddleware(app);
 configureRoutes(app);
 app.use(errorHandler);
+
+startServer(app);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,7 @@
-const app = require("../app");
 const MongooseDB = require("./config/database");
 const { PORT } = require("./config/environment");
 
-async function startServer() {
+async function startServer(app) {
     await MongooseDB.connect();
     const server = app.listen(PORT, () => {
         console.log(`Server started on port http://localhost:${PORT}`);
@@ -19,4 +18,4 @@ async function closeServer(server) {
     process.exit(0);
 }
 
-startServer();
+module.exports = {startServer};
